4ff9ba22fe17ba7e3a49cf83d1c53300b534cd2d
Shivansh Rai <shivansh@freebsd.org>
2018-01-22 16:46:18 +0530

    lexer: Avoid redundant inserts of lexemes into the hashtable

--- src/lexer.go
+++ src/lexer.go
@@ -1,3 +1,6 @@
+// Generate tokens and the corresponding
+// lexemes extracted from a source file.
+
 package main
 
 import (
@@ -22,8 +25,8 @@ func main() {
 	}
 
 	type TokInfo struct {
-		freq   int            // frequency of a lexeme
-		litMap map[string]int // stores "unique" lexemes of same type
+		freq   int             // frequency of a token
+		litMap map[string]bool // stores "unique" lexemes of same type
 	}
 
 	freqMap := make(map[token.Type]*TokInfo)
@@ -35,21 +38,24 @@ func main() {
 		}
 		lexeme := string(tok.Lit[:])
 		if freqMap[tok.Type] == nil {
+			// Allocate on first touch.
 			freqMap[tok.Type] = &TokInfo{
 				1,
-				map[string]int{lexeme: 1},
+				map[string]bool{lexeme: true},
 			}
 		} else {
-			freqMap[tok.Type].litMap[lexeme] = 1
 			freqMap[tok.Type].freq++
+			if !freqMap[tok.Type].litMap[lexeme] {
+				freqMap[tok.Type].litMap[lexeme] = true
+			}
 		}
 	}
 
 	fmt.Println("   Token         Occurrences        Lexemes")
 	fmt.Println("----------------------------------------------")
-	for k, v := range freqMap {
-		fmt.Printf("%12s%12d", token.TokMap.Id(k), v.freq)
-		for lexeme := range v.litMap {
+	for key, value := range freqMap {
+		fmt.Printf("%12s%12d", token.TokMap.Id(key), value.freq)
+		for lexeme := range value.litMap {
 			fmt.Printf("%18s\n\t\t\t", lexeme)
 		}
 		fmt.Printf("\r")
