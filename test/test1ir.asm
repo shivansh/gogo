75d100a1321f280f1d0305b26224c25304b69845
Shivansh Rai <shivansh@freebsd.org>
2018-02-04 18:57:00 +0530

    codegen: Make deterministic register allocation across different runs
    The range loop traverses a map in a non-deterministic order, and the
    register index returned by GetReg() relied on this behavior leading to
    different ordering of register allocation in the generated assembly
    across different runs (though the meaning was conserved). To avoid this
    we convert the O(1) algorithm to O(RegLimit) algorithm, and since
    RegLimit is known it is still O(1) but with slightly larger number of
    (constant) iterations than the previous version.

--- test/test1ir.asm
+++ test/test1ir.asm
@@ -7,27 +7,27 @@ v5:	.word	0
 
 	.text
 main:
-	la $t4, v1
-	li, $t1, -1		; v1 -> {reg: $t1, mem: $t4}
-	la $t2, v2
-	li, $t3, 2		; v2 -> {reg: $t3, mem: $t2}
-	li, $t1, -12
-	sw $t1, ($t4)		; spilled v1 and freed {$t1, $t4}
-	la $t1, v3
-	li, $t4, 3		; v3 -> {reg: $t4, mem: $t1}
-	sw $t4, ($t1)		; spilled v3 and freed {$t4, $t1}
-	la $t4, v4
-	li, $t1, 4		; v4 -> {reg: $t1, mem: $t4}
-	sw $t1, ($t4)		; spilled v4 and freed {$t1, $t4}
-	la $t1, v5
-	li, $t4, 5		; v5 -> {reg: $t4, mem: $t1}
+	la $t1, v1
+	li, $t2, -1		; v1 -> {reg: $t2, mem: $t1}
+	la $t3, v2
+	li, $t4, 2		; v2 -> {reg: $t4, mem: $t3}
+	li, $t2, -12
+	sw $t2, ($t1)		; spilled v1 and freed {$t2, $t1}
+	la $t2, v3
+	li, $t1, 3		; v3 -> {reg: $t1, mem: $t2}
+	sw $t1, ($t2)		; spilled v3 and freed {$t1, $t2}
+	la $t1, v4
+	li, $t2, 4		; v4 -> {reg: $t2, mem: $t1}
+	sw $t2, ($t1)		; spilled v4 and freed {$t2, $t1}
+	la $t2, v5
+	li, $t1, 5		; v5 -> {reg: $t1, mem: $t2}
 	jlt, $t0, 5, unsat
-	move, $t3, $t0
+	move, $t4, $t0
 	j temp
 
 	; Store variables back into memory
-	sw $t4, ($t1)
-	sw $t3, ($t2)
+	sw $t1, ($t2)
+	sw $t4, ($t3)
 temp:
 	jlt, $t0, $t0, unsat
 
