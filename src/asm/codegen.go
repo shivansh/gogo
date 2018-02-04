425469594d732b0660235266ae1915a296566b0a
Shivansh Rai <shivansh@freebsd.org>
2018-02-04 07:50:55 +0530

    Add logic for a basic register allocator (without "next-use" info)
    The implementation has a small bug hidden from sight somewhere causing
    an unexpected result.

--- src/asm/codegen.go
+++ src/asm/codegen.go
@@ -49,7 +49,8 @@ func CodeGen(t tac.Tac) {
 		// Register descriptor:
 		//	* Keeps track of what is currently in each register.
 		//	* Initially all registers are empty.
-		regDesc := make(map[int]string)
+		// regDesc := make(map[int]string)
+		blk.Rdesc = make(map[int]string)
 		// Address descriptor:
 		//	* Keeps track of location where current value of the
 		//	  name can be found at runtime.
@@ -57,7 +58,8 @@ func CodeGen(t tac.Tac) {
 		//		- register
 		//		- memory address
 		//		- stack (TODO)
-		addrDesc := make(map[string]AddrDesc)
+		// addrDesc := make(map[string]AddrDesc)
+		blk.Adesc = make(map[string]tac.AddrDesc)
 
 		// At the end of each basic block, all the registers are flushed
 		// back to memory which means that they can be reused inside a
@@ -86,40 +88,68 @@ func CodeGen(t tac.Tac) {
 			switch stmt.Op {
 			case "=":
 				if stmt.Src[0].Typ == "int" {
-					if addrDesc[stmt.Dst].reg == 0 {
+					_, ok := blk.Adesc[stmt.Dst]
+					// if blk.Adesc[stmt.Dst].Reg == 0 {
+					if !ok {
+						var tobespilled int
+						for k, _ := range blk.Rdesc {
+							tobespilled = k
+							break
+						}
+						addrIndex := blk.GetReg(tobespilled)
+						if addrIndex == tobespilled {
+							// Register was spilled
+							ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tsw $t%d, ($t%d)", tobespilled, blk.Adesc[blk.Rdesc[tobespilled]].Mem))
+							delete(blk.Rdesc, tobespilled)
+							delete(blk.Adesc, blk.Rdesc[tobespilled])
+						}
+
 						// Load variables from memory into registers
-						addrIndex := tac.GetReg()
 						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tla $t%d, %s", addrIndex, stmt.Dst))
-						regIndex := tac.GetReg()
+						// regIndex := blk.GetReg()
+						for k, _ := range blk.Rdesc {
+							tobespilled = k
+							break
+						}
+						regIndex := blk.GetReg(tobespilled)
+						if regIndex == tobespilled {
+							// Register was spilled
+							ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tsw $t%d, ($t%d)", tobespilled, blk.Adesc[blk.Rdesc[tobespilled]].Mem))
+							delete(blk.Rdesc, tobespilled)
+							delete(blk.Adesc, blk.Rdesc[tobespilled])
+						}
+
 						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tlw $t%d, ($t%d)", regIndex, addrIndex))
 						// Update lookup tables
-						regDesc[regIndex] = stmt.Dst
-						addrDesc[stmt.Dst] = AddrDesc{regIndex, addrIndex}
+						blk.Rdesc[regIndex] = stmt.Dst
+						blk.Adesc[stmt.Dst] = tac.AddrDesc{regIndex, addrIndex}
 						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tli, $t%d, %s", regIndex, stmt.Src[0].Val))
 					} else {
-						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tli, $t%d, %s", addrDesc[stmt.Dst].reg, stmt.Src[0].Val))
+						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tli, $t%d, %s", blk.Adesc[stmt.Dst].Reg, stmt.Src[0].Val))
 					}
 				} else {
-					if addrDesc[stmt.Dst].reg == 0 {
+					if blk.Adesc[stmt.Dst].Reg == 0 {
+						addrIndex := blk.GetReg(blk.Adesc[stmt.Src[0].Val].Reg)
+
 						// Load variables from memory into registers
-						addrIndex := tac.GetReg()
+						addrIndex = blk.GetReg(blk.Adesc[stmt.Src[0].Val].Reg)
 						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tla $t%d, %s", addrIndex, stmt.Dst))
-						regIndex := tac.GetReg()
-						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tlw $t%d, ($t%d)", regIndex, addrIndex))
+						regIndex := blk.GetReg(blk.Adesc[stmt.Src[0].Val].Reg)
+						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tpingilw $t%d, ($t%d)", regIndex, addrIndex))
 						// Update lookup tables
-						regDesc[regIndex] = stmt.Dst
-						addrDesc[stmt.Dst] = AddrDesc{regIndex, addrIndex}
-						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tmove, $t%d, $t%d", regIndex, addrDesc[stmt.Src[0].Val].reg))
+						blk.Rdesc[regIndex] = stmt.Dst
+						blk.Adesc[stmt.Dst] = tac.AddrDesc{regIndex, addrIndex}
+						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tmove, $t%d, $t%d", regIndex, blk.Adesc[stmt.Src[0].Val].Reg))
 					} else {
-						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tmove, $t%d, $t%d", addrDesc[stmt.Dst].reg, addrDesc[stmt.Src[0].Val].reg))
+						ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tmove, $t%d, $t%d", blk.Adesc[stmt.Dst].Reg, blk.Adesc[stmt.Src[0].Val].Reg))
 					}
 				}
 			case "<":
 				if stmt.Src[0].Typ == "int" {
-					ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tjlt, $t%d, %s, %s", addrDesc[stmt.Dst].reg, stmt.Src[0].Val, stmt.Src[1].Val))
+					ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tjlt, $t%d, %s, %s", blk.Adesc[stmt.Dst].Reg, stmt.Src[0].Val, stmt.Src[1].Val))
 				} else {
 					ts.Stmts = append(ts.Stmts,
-						fmt.Sprintf("\tjlt, $t%d, $t%d, %s", addrDesc[stmt.Dst].reg, addrDesc[stmt.Src[0].Val].reg, stmt.Src[1].Val))
+						fmt.Sprintf("\tjlt, $t%d, $t%d, %s", blk.Adesc[stmt.Dst].Reg, blk.Adesc[stmt.Src[0].Val].Reg, stmt.Src[1].Val))
 				}
 			case "label":
 				ts.Stmts = append(ts.Stmts, fmt.Sprintf("%s:", stmt.Dst))
@@ -134,11 +164,21 @@ func CodeGen(t tac.Tac) {
 		ts.Stmts = append(ts.Stmts, "\n\t; Store variables back into memory")
 
 		// Only store the variables which were loaded in the first place
-		for _, v := range addrDesc {
-			if v.mem > 0 {
-				ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tsw $t%d, ($t%d)", v.reg, v.mem))
+		for _, v := range blk.Adesc {
+			if v.Mem > 0 {
+				ts.Stmts = append(ts.Stmts, fmt.Sprintf("\tsw $t%d, ($t%d)", v.Reg, v.Mem))
 			}
 		}
+
+		// Update the symbol tables inside the basic block
+		// blk.Adesc = make(map[string]tac.AddrDesc)
+		// blk.Rdesc = make(map[int]string)
+		// for k, v := range addrDesc {
+		// 	blk.Adesc[k] = tac.AddrDesc{v.reg, v.mem}
+		// }
+		// for k, v := range regDesc {
+		// 	blk.Rdesc[k] = v
+		// }
 	}
 
 	ds.Stmts = append(ds.Stmts, "") // data section terminator
--- test/test1ir.asm
+++ test/test1ir.asm
@@ -1,31 +1,43 @@
 	.data
 v1:	.word	0
 v2:	.word	0
+v3:	.word	0
 v4:	.word	0
+v5:	.word	0
 
 	.text
 main:
-	la $t1, v1
-	lw $t2, ($t1)
+	la $t2, v1
+	lw $t2, ($t2)
 	li, $t2, -1
-	li, $t2, 2
-	jlt, $t2, 5, unsat
-	la $t3, v2
-	lw $t4, ($t3)
-	move, $t4, $t2
+	la $t0, v2
+	lw $t0, ($t0)
+	li, $t0, 2
+	la $t0, v3
+	lw $t0, ($t0)
+	li, $t0, 3
+	la $t0, v4
+	lw $t0, ($t0)
+	li, $t0, 4
+	la $t0, v5
+	lw $t0, ($t0)
+	li, $t0, 5
+	jlt, $t0, 5, unsat
+	la $t0, v2
+	pingilw $t0, ($t0)
+	move, $t0, $t2
 	j temp
 
 	; Store variables back into memory
-	sw $t2, ($t1)
-	sw $t4, ($t3)
+	sw $t2, ($t2)
 temp:
 	jlt, $t0, $t0, unsat
 
 	; Store variables back into memory
 unsat:
-	la $t1, v4
-	lw $t2, ($t1)
+	la $t2, v4
+	lw $t2, ($t2)
 	li, $t2, 4
 
 	; Store variables back into memory
-	sw $t2, ($t1)
+	sw $t2, ($t2)
