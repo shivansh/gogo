93d69a1c79e2c98ac6d7378b5dd19775dfc1bec0
Shivansh Rai <shivansh@freebsd.org>
2018-03-18 22:34:59 +0530

    parser: Collate all routines
    Co-authored-by: kpsunil <sunilpandey.pandey09@gmail.com>
    Co-authored-by: tvatsal1996 <tvatsal1996@gmail.com>

--- src/main.go
+++ src/main.go
@@ -5,18 +5,30 @@ import (
 	"os"
 
 	"gogo/src/asm"
+	"gogo/src/parser"
 	"gogo/src/tac"
 )
+
+// GenAsm generates the assembly code using the IR generated from the input program.
+func GenAsm(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	asm.CodeGen(tac.GenTAC(f))
+}
+
+// GenRightmostDerivation generates the rightmost derivations used in the bottom-up
+// parsing and pretty-prints them in an HTML format.
+func GenRightmostDerivation(file string) {
+	parser.GenHTML(file)
+}
 
 func main() {
 	args := os.Args
 	if len(args) != 2 {
-		log.Fatalf("Usage: gogo ir-file")
+		log.Fatalf("Usage: gogo <filename>")
 	}
-	file, err := os.Open(args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	asm.CodeGen(tac.GenTAC(file))
-	file.Close()
+	GenRightmostDerivation(args[1])
 }
