8a6b8592939eb1211c7d3bfb22a3085fd4eaf0a5
tvatsal1996 <tvatsal1996@gmail.com>
2018-03-19 21:35:45 +0530

    parser: Add more tests

---
+++ test/parser/conditionals.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 2
+	if x != 2 {
+		fmt.Println(x)
+	}
+}
---
+++ test/parser/declarations.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int = 2, 3
+	const (
+		z       int  = 2
+		isempty bool = true
+	)
+	type (
+		switches      int
+		conditiontype bool
+	)
+}
