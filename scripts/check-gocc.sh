e5dd70d3326ed002d7ecb17d46213db2d7a98604
Aniket Pandey <aniketp@iitk.ac.in>
2018-03-24 18:26:30 +0530

    Add a helper script for 'make deps'

---
+++ scripts/check-gocc.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Script to generate temporary tokens 
+
+SRC='./src'
+tmp='tmp'
+
+mkdir -p "$tmp"
+
+if [ -z "$GOPATH" ]; then
+    echo "GOPATH environment variable is not set"
+    exit 1
+else
+    goccpath="$GOPATH"/bin/gocc
+    if [ -f "$goccpath" ]; then
+        gocc -a -zip -o "$tmp" "$SRC"/lang.bnf
+    else
+        echo "gocc is not properly installed"
+        exit 1
+    fi
+fi
+
