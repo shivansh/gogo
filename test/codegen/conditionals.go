8c83f0515b061d1496bc92673ef68ac60bd0f507
Shivansh Rai <shivansh@freebsd.org>
2018-04-03 22:33:01 +0530

    codegen: Handle remaining if-else statement combinations
    Co-authored-by: kpsunil <sunilpandey.pandey09@gmail.com>

--- test/codegen/conditionals.go
+++ test/codegen/conditionals.go
@@ -1,10 +1,13 @@
 func main() {
-        a := 1
-        b := 1
-        if a + b {
-                a := 2
-        } else {
-                b := 3
-        }
-        b := 4 + 2
+	a := 1
+	b := 1
+	if a + b {
+		a := 2
+	} else if b > 2 {
+		b := 3
+	}
+	if c := 1; c < 2 {
+		c = 3
+	}
+	b := 4 + 2
 }
