fb1550b5c415db81e4b08bc8f5ee0bd556bf30fe
Shivansh Rai <shivansh@freebsd.org>
2018-04-06 00:51:53 +0530

    codegen: Handle scopes using symbol tables
    Fixes #7
    Fixes #8
    
    Co-authored-by: kpsunil <sunilpandey.pandey09@gmail.com>
    Co-authored-by: tvatsal1996 <tvatsal1996@gmail.com>

--- test/codegen/arrays.go
+++ test/codegen/arrays.go
@@ -1,10 +1,31 @@
 func main() {
 	a := [2]int{}
-	a[1] = 1
+	a[0] = 1
 	for {
-		if a[1] == 2 {
+		if a[0] == 2 {
 			break
 		}
 	}
 	return
 }
+
+// func, main
+// decl, a, 2
+// from, t0, a, 1
+// =, t0, 1
+// into, a, a,  1, t0
+// label, l3
+// from, t1, a, 1
+// printInt, t1, t1
+// bne, l0, t1, 2
+// =, t2, 1
+// j, l1
+// label, l0
+// =, t2, 0
+// label, l1
+// blt, l2, t2, 1
+// j, l4
+// label, l2
+// j, l3
+// label, l4
+// ret,
