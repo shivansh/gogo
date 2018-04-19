d4d446e419a617f6be018aa3852735046128ba1a
Shivansh Rai <shivansh@freebsd.org>
2018-04-19 14:39:06 +0530

    codegen: Add constant declarations in symbol table ; Fix tests

--- test/codegen/declarations.go
+++ test/codegen/declarations.go
@@ -1,15 +1,14 @@
-const a
+func temp() {
+	return 1
+}
 
 func main() {
 	const b = 1 + 2 + 3
 	var c int
 	var d int = 1 + 2
-        x := 1 + 2
-        a + b++
-        c := temp()
-        return a + b
-}
-
-func temp() {
-        return 1
+	x := 1 + 2
+	a := b + 1
+	// a := b++
+	c := temp()
+	return a + b
 }
